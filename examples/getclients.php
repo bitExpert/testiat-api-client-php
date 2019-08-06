2e866d8ea8f44778e2763a4c2f5cfd252e7dc619
Daniel Ruf <daniel@daniel-ruf.de>
2019-08-06 21:38:10 +0200

    Use single quotes

--- examples/getclients.php
+++ examples/getclients.php
@@ -7,7 +7,7 @@ require '../vendor/autoload.php';
 $factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 $client = new \Buzz\Client\Curl($factory);
 
-$api = new Api($client, $factory, "someapikey");
+$api = new Api($client, $factory, 'someapikey');
 
 $response = $api->getAvailableClients();
 $response = $response->getBody();
