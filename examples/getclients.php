3f647c455797a766c2533640ada26899f619e28c
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2019-08-09 21:07:34 +0200

    Introduce proper namespace & autoloading support

--- examples/getclients.php
+++ examples/getclients.php
@@ -2,7 +2,7 @@
 
 use \bitExpert\Testiat\Api;
 
-require dirname(__DIR__, 1) . '/testiat.php';
+require '../vendor/autoload.php';
 
 $factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 $client = new \Buzz\Client\Curl($factory);
